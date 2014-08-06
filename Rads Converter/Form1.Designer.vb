32dafd5382a13862ca1b2021d96317feeabc9e49
Foxtrek_64 <foxtrek64@gmail.com>
2014-08-06 13:46:32 -0500

    Initial Commit
    Convert Radiants to Emeralds and Back

---
+++ Rads Converter/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(109, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(149, 25)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(109, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(127, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "+"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(264, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "="
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(286, 25)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(407, 24)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 20)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Convert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(13, 52)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(125, 17)
+        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Emeralds to Radiants"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 75)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(125, 17)
+        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Radiants to Emeralds"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 98)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(136, 17)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Consolidate into Blocks"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(154, 51)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(232, 68)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(407, 52)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 19)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Emerald Blocks"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(149, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Emeralds"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(286, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Radiants"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.License
+        Me.PictureBox1.Location = New System.Drawing.Point(393, 78)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 11
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 129)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Rad Converter"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
---
+++ Rads Converter/Form1.vb
@@ -0,0 +1,160 @@
+﻿Public Class Form1
+
+    Dim Blocks As Integer
+    Dim BlockEmeralds As Integer
+    Dim Emeralds As Double
+    Dim TotalEmeralds As Integer
+    Dim Rads As Double
+    Dim DoEmeralds As Boolean
+
+    Private Sub RadioButton1_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        'Emeralds to Radiants
+        If RadioButton1.Checked = True Then
+            RadioButton2.Checked = False
+            CheckBox1.Checked = False
+            CheckBox1.Enabled = False
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles RadioButton2.CheckedChanged
+        'Radiants to Emeralds
+        If RadioButton2.Checked = True Then
+            RadioButton1.Checked = False
+            CheckBox1.Enabled = True
+            TextBox1.Text = ""
+            TextBox2.Text = ""
+            TextBox3.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+
+        If RadioButton1.Checked = True Then 'Emeralds to Rads
+            Dim Valid As Boolean
+
+            If TextBox1.Text.ToString = "" Then
+                Blocks = 0
+                TextBox4.AppendText("Emerald Blocks: " + Blocks.ToString + vbCrLf)
+            Else
+                Valid = vbNull
+                Valid = Int32.TryParse(TextBox1.Text, Blocks)
+                If Valid = True Then
+                    Blocks = Int32.Parse(TextBox1.Text)
+                    TextBox4.AppendText("Emerald Blocks: " + Blocks.ToString + vbCrLf)
+                ElseIf Valid = False Then
+                    MsgBox("You must input an integer value into the Emerald Block Field.", MsgBoxStyle.Exclamation, "Integer value required")
+                End If
+            End If
+
+            If TextBox2.Text.ToString = "" Then
+                Emeralds = 0
+                TextBox4.AppendText("Emeralds: " + Emeralds.ToString + vbCrLf)
+            Else
+                Valid = vbNull
+                Valid = Int32.TryParse(TextBox2.Text, Emeralds)
+                If Valid = True Then
+                    Emeralds = Int32.Parse(TextBox2.Text)
+                    TextBox4.AppendText("Emeralds: " + Emeralds.ToString + vbCrLf)
+                ElseIf Valid = False Then
+                    MsgBox("You must input an integer value into the Emerald field.", MsgBoxStyle.Exclamation, "Integer value required")
+                End If
+            End If
+
+            'If Int32.TryParse(TextBox1.Text, Blocks) = True Then
+            'If Int32.TryParse(TextBox2.Text, Emeralds) = True Then
+            BlockEmeralds = Math.BigMul(Blocks, 9)
+
+            TextBox4.AppendText(Blocks.ToString + " times 9 = " + BlockEmeralds.ToString + vbCrLf)
+            TotalEmeralds = BlockEmeralds + Emeralds
+
+            TextBox4.AppendText("Total number of Emeralds: " + TotalEmeralds.ToString + vbCrLf)
+
+            TextBox4.AppendText("Multiplying " + TotalEmeralds.ToString + " Emeralds by 20" + vbCrLf)
+
+            Rads = Math.BigMul(TotalEmeralds, 20)
+
+            TextBox3.Text = Rads
+
+            TextBox4.AppendText("Rads: " + Rads.ToString + vbCrLf)
+            'End If
+            'End If
+        End If
+
+        If RadioButton2.Checked = True Then 'Rads to Emeralds
+            Dim Valid As Boolean
+            Dim Rads As Integer
+            Dim Remainder As Integer
+            Dim Emeralds As Integer
+            Dim Blocks As Integer
+
+
+            If TextBox3.Text.ToString = "" Then
+                Rads = 0
+                MsgBox("Please insert the number of Radiants you would like to convert.", MsgBoxStyle.Exclamation, "Insert Radiants")
+            Else
+                Valid = Int32.TryParse(TextBox3.Text, Rads)
+
+                If Valid = True Then
+                    TextBox4.AppendText("Dividing " + Rads.ToString + " Emeralds by 20" + vbCrLf)
+                    Emeralds = Math.DivRem(Rads, 20, Remainder)
+
+                    If Remainder = 0 Then
+                        TextBox4.AppendText("There are " + Emeralds.ToString + " Emeralds")
+                    ElseIf Remainder > 0 Then
+                        TextBox4.AppendText("There are " + Emeralds.ToString + " Emeralds and " + Remainder.ToString + " Radiants remaining.")
+                    End If
+
+                    If DoEmeralds = True Then
+                        Blocks = Math.DivRem(Emeralds, 9, Remainder)
+                        TextBox1.Text = Blocks
+                        TextBox2.Text = Remainder
+                    Else
+                        TextBox1.Text = ""
+                        TextBox2.Text = Emeralds
+                    End If
+
+                ElseIf Valid = False Then
+                    MsgBox("You must input an integer value into the Radiants field.", MsgBoxStyle.Exclamation, "Integer value required")
+                End If
+            End If
+
+        End If
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        TextBox4.ResetText()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            DoEmeralds = True
+        ElseIf CheckBox1.Checked = False Then
+            DoEmeralds = False
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As System.Object, e As System.EventArgs) Handles PictureBox1.Click
+        Dim license = MsgBox("This program is packaged with a Creative Commons 'Attribution-NonCommercial-ShareAlike 4.0 International" + (Chr(13) & Chr(10)) + "(CC BY-NC-SA 4.0)' License." + (Chr(13) & Chr(10)) + (Chr(13) & Chr(10)) + "Information can be found at http://creativecommons.org/licenses/by-nc-sa/4.0/. Would you like to view this page?", MsgBoxStyle.Information & MsgBoxStyle.YesNo, "Licensing")
+        If license = MsgBoxResult.Yes Then
+            Dim Location As String = "http://creativecommons.org/licenses/by-nc-sa/4.0/"
+            Process.Start(Location)
+        ElseIf license = MsgBoxResult.No Then
+            'Terminate
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        TextBox4.AppendText("Author: Devin Duanne (Foxtrek_64).")
+        TextBox4.AppendText("License: CC BY-NC-SA 4.0 International")
+    End Sub
+End Class
